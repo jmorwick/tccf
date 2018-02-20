8948f6eb6db6a939dd604ade7f7f954500c1806b
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2018-02-19 23:19:32 -0600

    Updated form controller to pull colony submissions

--- colony-form-module/src/Controller/ColonyFormController.php
+++ colony-form-module/src/Controller/ColonyFormController.php
@@ -3,10 +3,28 @@
 namespace Drupal\colony_form\Controller;
 
 class ColonyFormController {
-    public function hello() {
-        return array(
-                '#title' => 'Hello World!',
-                '#markup' => 'Here is some content.',
-            );
+    public function colonies() {
+      // webform with colony submissions
+      $form_node_title = 'Members Only'; 
+
+      // find node with webform
+      $node = \Drupal::entityTypeManager()
+                 ->getStorage('node')
+                 ->loadByProperties(['title' => $form_node_title]);
+
+      // find form submissions
+      $storage = \Drupal::entityTypeManager()->getStorage('webform_submission');
+      $submissions = $storage->loadByProperties([
+        'entity_type' => 'node',
+        'entity_id' => array_keys($node)[0]
+      ]);
+      $map_data = array();
+      foreach ($submissions as $submission) {
+        $map_data[] = $submission->getData();
+      }
+      return array(
+        '#title' => 'Hello World!',
+        '#markup' => print_r($map_data,true)
+      );
     }
 }
